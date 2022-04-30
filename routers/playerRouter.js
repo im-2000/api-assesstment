4d931a7d1f4679b81a63cfbdcf4f72222a24c5de
hardcorebuddhist <im.energo@gmail.com>
2022-04-30 16:40:24 +0200

    f4 except #3

--- routers/playerRouter.js
+++ routers/playerRouter.js
@@ -3,12 +3,27 @@ const router = new Router();
 const Player = require("../models").player;
 // const bcrypt = require("bcrypt");
 
+// GET ALL PLAYERS
+
 router.get("/", async (req, res, next) => {
   try {
     res.send(await Player.findAll());
   } catch (e) {
     console.log(e);
     next(e);
+  }
+});
+
+// CREATE NEW PLAYER
+
+router.post("/", async (request, response, next) => {
+  try {
+    const { name, age } = request.body;
+    const newPlayer = await Player.create({ name, age });
+    response.send(newPlayer);
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 });
 
--- routers/teamRouter.js
+++ routers/teamRouter.js
@@ -1,15 +1,57 @@
 const { Router } = require("express");
 const router = new Router();
 const Team = require("../models").team;
+const Player = require("../models/player");
 // const bcrypt = require("bcrypt");
 
-router.get("/", async (req, res, next) => {
+// // GET ALL TEAMS
+
+// router.get("/", async (req, res, next) => {
+//   try {
+//     res.send(await Team.findAll());
+//   } catch (e) {
+//     console.log(e);
+//     next(e);
+//   }
+// });
+
+// GET a specific team, including all its players
+
+router.get("/:id", async (req, res, next) => {
   try {
-    res.send(await Team.findAll());
-  } catch (e) {
-    console.log(e);
-    next(e);
+    const specificTeam = await Team.findByPk(req.params.id);
+
+    if (!specificTeam) {
+      res.status(404).send(`User with id ${req.params.id} not found`);
+    } else {
+      res.send(specificTeam);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 });
+
+// // UPDATE TITLES OF TEAM #3
+
+// router.patch("/:id", async (request, response, next) => {
+//   try {
+//     const { titles } = request.body;
+//     const { id } = request.params;
+
+//     const team = await Team.findByPk(id);
+
+//     const updatedTeam = await team.update({ titles });
+
+//     if (!updatedTeam) {
+//       res.status(404).send(`User with id ${request.params.id} not found`);
+//     } else {
+//       response.send(updatedTeam);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// });
 
 module.exports = router;
